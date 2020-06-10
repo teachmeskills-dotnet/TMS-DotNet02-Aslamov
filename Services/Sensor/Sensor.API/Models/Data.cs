0d464cac5b0487ae918ace87c44fd4224ce4c7af
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-10 12:33:39 +0300

    feat: add sensor entities

---
+++ Services/Sensor/Sensor.API/Models/Data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Sensor.API.Models
+{
+    /// <summary>
+    /// Sensor data entity.
+    /// </summary>
+    public class Data
+    {
+        /// <summary>
+        /// Data identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Data value.
+        /// </summary>
+        public byte[] Value { get; set; }
+
+        /// <summary>
+        /// Data acquisition  date.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Mark deleted data.
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+
+        /// <summary>
+        /// Sensor identifier.
+        /// </summary>
+        public int SensorId { get; set; }
+
+        /// <summary>
+        /// Sensor.
+        /// </summary>
+        public Sensor Sensor { get; set; }
+    }
+}
---
+++ Services/Sensor/Sensor.API/Models/Sensor.cs
@@ -0,0 +1,18 @@
+﻿namespace Sensor.API.Models
+{
+    /// <summary>
+    /// Sensor entity.
+    /// </summary>
+    public class Sensor
+    {
+        /// <summary>
+        /// Sensor Identifier.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Sensor serial number.
+        /// </summary>
+        public string Serial { get; set; }
+    }
+}

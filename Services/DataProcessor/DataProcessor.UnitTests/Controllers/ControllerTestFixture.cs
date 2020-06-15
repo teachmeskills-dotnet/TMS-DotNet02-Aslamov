534f26adac8708f87397203597e6c20c218d0c85
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-15 10:09:48 +0300

    test: add xUnit texts for DataProcessorController

---
+++ Services/DataProcessor/DataProcessor.UnitTests/Controllers/ControllerTestFixture.cs
@@ -0,0 +1,43 @@
+﻿using DataProcessor.API.DTO;
+using System;
+
+namespace DataProcessor.UnitTests.Controllers
+{
+    /// <summary>
+    /// Define base functions for controller tests.
+    /// </summary>
+    public class ControllerTestFixture
+    {
+        /// <summary>
+        /// Generate single Report DTO.
+        /// </summary>
+        /// <returns>Report DTO.</returns>
+        public ReportDTO GetReport()
+        {
+            return new ReportDTO()
+            {
+                Date = DateTime.Now,
+                HealthStatus = "Healthy",
+                SensorDeviceId = 1,
+                SensorDeviceSerial = "123456789",
+                SensorDeviceType = "Acoustic",
+            };
+        }
+
+        /// <summary>
+        /// Generate single Data DTO.
+        /// </summary>
+        /// <returns>Data DTO.</returns>
+        public DataDTO GetData()
+        {
+            return new DataDTO()
+            {
+                Date = DateTime.Now,
+                SensorDeviceId = 1,
+                SensorDeviceSerial = "123456789",
+                SensorDeviceType = "Acoustic",
+                Value = new byte[] { 255, 254, 253, 252 },
+            };
+        }
+    }
+}
--- Services/Sensor/Sensor.API/Program.cs
+++ Services/Sensor/Sensor.API/Program.cs
@@ -1,7 +1,5 @@
 using System;
-using System.IO;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Sensor.API.Common.Extensions;
 using Sensor.API.Common.Interfaces;

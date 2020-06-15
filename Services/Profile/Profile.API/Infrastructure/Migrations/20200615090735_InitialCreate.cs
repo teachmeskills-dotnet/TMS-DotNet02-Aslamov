099fa982d3cf111b5d2455eb1ea33eb112c34702
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-15 12:08:50 +0300

    fead: creat initial migration for Profile.API

---
+++ Services/Profile/Profile.API/Infrastructure/Migrations/20200615090735_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Profile.API.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 25, nullable: false),
+                    LastName = table.Column<string>(maxLength: 20, nullable: false),
+                    MiddleName = table.Column<string>(maxLength: 20, nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<string>(maxLength: 20, nullable: true),
+                    Height = table.Column<int>(nullable: false, defaultValue: 170),
+                    Weight = table.Column<int>(nullable: false, defaultValue: 60)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profiles");
+        }
+    }
+}

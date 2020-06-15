7c72a660e94a474dc4ee75106fbb8dc915567b7f
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-15 17:34:42 +0300

    feat: add main entities for Identity.API

---
+++ Services/Identity/Identity.API/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿namespace Identity.API.Models
+{
+    /// <summary>
+    /// Entity for login .
+    /// </summary>
+    public class Login
+    {
+        /// <summary>
+        /// User email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// User password.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}

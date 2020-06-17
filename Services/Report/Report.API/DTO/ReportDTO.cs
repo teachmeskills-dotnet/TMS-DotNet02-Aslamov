6e8085fd50c1fa32ce7f2d9057cda8d4e945fd77
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-17 17:00:40 +0300

    feat: add entities for Report.API

---
+++ Services/Report/Report.API/DTO/ReportDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Report.API.DTO
+{
+    /// <summary>
+    /// Report Data Transfer Object (DTO).
+    /// </summary>
+    public class ReportDTO
+    {
+        /// <summary>
+        /// User profile identifier.
+        /// </summary>
+        public int ProfileId { get; set; }
+
+        /// <summary>
+        /// Sensor identifier.
+        /// </summary>
+        public int SensorDeviceId { get; set; }
+
+        /// <summary>
+        /// Data acquisition date.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Health status of the patient.
+        /// </summary>
+        public string HealthStatus { get; set; }
+    }
+}

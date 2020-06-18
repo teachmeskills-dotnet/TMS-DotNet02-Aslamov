945b9fe49ec85a291b330cd62ed18e62d39913ab
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-18 14:10:11 +0300

    feat: docker-compose for Sensor.API

--- Services/Sensor/Sensor.API/Startup.cs
+++ Services/Sensor/Sensor.API/Startup.cs
@@ -1,29 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Sensor.API.Common.Extensions;
-using Sensor.API.Infrastructure;
 
 namespace Sensor.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+
+        public IHostEnvironment Environment { get;  }
+
+        public Startup(IConfiguration configuration, IHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
 
-            services.AddDbContext<SensorContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddApplicationDbContext(Configuration, Environment);
 
             services.AddAutomapper();
             services.AddScopedServices();
@@ -33,7 +33,7 @@ namespace Sensor.API
             services.AddHealthChecks();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,7 +46,7 @@ namespace Sensor.API
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "iCare Sensor API version 1"));
 
-            //app.UseAuthentication(); //TODO: Uncomment after implementing the identity service!
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {

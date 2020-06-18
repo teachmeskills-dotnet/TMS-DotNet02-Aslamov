f399f90a80ff26bf60bb864c364309fa233cba66
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-18 20:13:43 +0300

    feat: add Ocelot gateway for Gateway.API

--- ApiGateways/Gateway.API/Startup.cs
+++ ApiGateways/Gateway.API/Startup.cs
@@ -3,14 +3,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace Gateway.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostEnvironment environment)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            //Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -18,6 +26,9 @@ namespace Gateway.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddOcelot(Configuration);
+
+            services.AddHealthChecks();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,11 +39,14 @@ namespace Gateway.API
             }
 
             app.UseRouting();
-            app.UseAuthorization();
+
+            //app.UseAuthorization();
+            app.UseOcelot().Wait();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/hc");
             });
         }
     }

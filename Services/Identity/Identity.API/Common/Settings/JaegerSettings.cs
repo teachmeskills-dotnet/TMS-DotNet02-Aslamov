b74f64ad86f48a03ff7ac083b9b5be4e357060c3
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-24 19:05:37 +0300

    fix: resolve issues with Jaeger for Identity.API

---
+++ Services/Identity/Identity.API/Common/Settings/JaegerSettings.cs
@@ -0,0 +1,33 @@
+﻿namespace Identity.API.Common.Settings
+{
+    /// <summary>
+    /// Jaeger service settings.
+    /// </summary>
+    public class JaegerSettings
+    {
+        /// <summary>
+        /// Name of tracing service (in Jaeger UI).
+        /// </summary>
+        public string ServiceName { get; set; }
+
+        /// <summary>
+        /// Default agent host (Development environment).
+        /// </summary>
+        public string DefaultAgentHost { get; set; }
+
+        /// <summary>
+        /// Docker agent host (Production environment).
+        /// </summary>
+        public string DockerAgentHost { get; set; }
+
+        /// <summary>
+        /// Jaeger agent port.
+        /// </summary>
+        public string AgentPort { get; set; }
+
+        /// <summary>
+        /// Sampler type.
+        /// </summary>
+        public string SamplerType { get; set; }
+    }
+}
